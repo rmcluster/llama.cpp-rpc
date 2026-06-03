bb67afa1fc8bc5fa9f4180a568068073cdb3283c
angelajzhou <155860768+angelajmzhou@users.noreply.github.com>
2026-06-02 20:53:30 -0700

    add stop request

--- ggml/include/ggml-rpc.h
+++ ggml/include/ggml-rpc.h
@@ -26,6 +26,7 @@ GGML_BACKEND_API void ggml_backend_rpc_get_device_memory(const char * endpoint, 
 
 GGML_BACKEND_API void ggml_backend_rpc_start_server(const char * endpoint, const char * cache_dir,
                                                     size_t n_threads, size_t n_devices, ggml_backend_dev_t * devices);
+GGML_BACKEND_API bool ggml_backend_rpc_stop_server(const char * endpoint);
 
 GGML_BACKEND_API ggml_backend_reg_t ggml_backend_rpc_reg(void);
 GGML_BACKEND_API ggml_backend_reg_t ggml_backend_rpc_add_server(const char * endpoint);
